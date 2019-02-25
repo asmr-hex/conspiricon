8d2208b95f3cd5dfd42c63e8052e07583d59238d
connorwalsh <c@polygon.pizza>
2019-02-24 23:36:18 -0500

    preliminary styles

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return render_template('index.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
